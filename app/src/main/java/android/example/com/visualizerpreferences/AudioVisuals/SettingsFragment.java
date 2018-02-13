a781bedf98337658b563046b7863e7fb936fe522
rungene <rungenesamuel@gmail.com>
2018-02-13 19:40:41 +0300

    Making Preference Fragment: Working on Settings Fragment,styles.xml files and pref_visualizer.xml files

---
+++ app/src/main/java/android/example/com/visualizerpreferences/AudioVisuals/SettingsFragment.java
@@ -0,0 +1,20 @@
+package android.example.com.visualizerpreferences.AudioVisuals;
+
+import android.example.com.visualizerpreferences.R;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by SAM on 2/13/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+
+
+    }
+}
